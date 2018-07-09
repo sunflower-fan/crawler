28661b6cc6c6df0c0d8c89edd75291e641ffb78c
fanlw <fanlw2@derbysoft.com>
2018-07-09 22:02:23 +0800

    spider.py ing

--- core/spider.py
+++ core/spider.py
@@ -6,10 +6,16 @@ from core.scheduler import DefaultScheduler
 class Spider(object):
 
     def __init__(self, page_processor) -> None:
+        self.request
+        self.coroutineNum
         self.pageProcessor = page_processor
         self.downloader = DefaultDownloader()
         self.pipeline = DefaultPipeline()
         self.scheduler = DefaultScheduler()
 
     def start(self):
-        pass
+        self.scheduler.push(self.request)
+        while True:
+            todo_request = self.scheduler.poll()
+            # download page
+            self.pageProcessor.process(todo_request)
